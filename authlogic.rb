2d963e7514b51452edacdcf2a97ee86e4a20b75e
Billy Catherall <billy.catherall@dom-itech.com>
2009-08-29 18:21:38 -0500

    fixed typoes fixed quoting on git commit fixed lib on oid

--- authlogic.rb
+++ authlogic.rb
@@ -2,23 +2,24 @@
   load_template "http://github.com/bcatherall/dp-rails_app_templates/raw/master/base.rb"
 
 #Install Gems
-  gem 'authlogic'
-  gem 'authlogic-oid'
+  gem 'authlogic-oid', :lib => 'authlogic_openid'
   gem 'ruby-openid', :lib => 'openid'
+  gem 'authlogic'
+  rake("gems:install", :sudo => true)
 
 #Initialize user and session scaffolds
-  generate (:nifty_scaffold, "user username:string email:string password:string new edit")
+  generate(:nifty_scaffold, "user username:string email:string password:string new edit")
   rake ("db:migrate")
-  generate (:session, "user_session")
-  generate (:nifty_scaffold, "user_session --skip-model username:string password:string new destroy")
+  generate(:session, "user_session")
+  generate(:nifty_scaffold, "user_session --skip-model username:string password:string new destroy")
 
 #Add open id
   plugin "open_id_authentication", :git => "git://github.com/rails/open_id_authentication.git", :submodule => true
 
 #Initialize open id  
-  rake ("open_id_authentication:db:create")
-  generate (:migration, "add_openid_identifier_to_users openid_identifier:string")
-  rake ("db:migrate")
+  rake("open_id_authentication:db:create")
+  generate(:migration, "add_openid_identifier_to_users openid_identifier:string")
+  rake("db:migrate")
 
 #Add Routes
   route 'map.login "login", :controller => "user_sessions", :action => "new"' 
@@ -31,7 +32,7 @@
 
 #### Add and Commit all changes
   git :add => '.'
-  git :commit => "-a -m 'Initial commit -  from http://github.com/bcatherall/dp-rails_app_templates/raw/master/base.rb'"
+  git :commit => '-m "Initial commit -  from http://github.com/bcatherall/dp-rails_app_templates/raw/master/base.rb"'
 
 # Success
   puts "SUCCESS!"
