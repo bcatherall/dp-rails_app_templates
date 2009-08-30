9741f08e3aa8d282e6e2322109509de9de0efc37
Billy Catherall <billy.catherall@dom-itech.com>
2009-08-30 12:21:52 -0500

    fixed typoes removed base.rb requirement

--- ecommerce.rb
+++ ecommerce.rb
@@ -1,7 +1,6 @@
 #load base templates
-  load_template "http://github.com/bcatherall/dp-rails_app_templates/raw/master/base.rb"
-  load_template "http://github.com/bcatherall/dp-rails_app_templates/raw/master/authentication.rb" if yes? ("User restful-authentication?")
-  load_template "http://github.com/bcatherall/dp-rails_app_templates/raw/master/suthlogic.rb" if yes? ("User authlogic?")
+  load_template "http://github.com/bcatherall/dp-rails_app_templates/raw/master/authentication.rb" if yes? ("Use restful-authentication?")
+  load_template "http://github.com/bcatherall/dp-rails_app_templates/raw/master/authlogic.rb" if yes? ("Use authlogic?")
 
 # Add Active merchant
   gem 'activemerchant'
@@ -9,7 +8,7 @@
 
 #### Add and Commit all changes
   git :add => '.'
-  git :commit => "-a -m 'Initial commit -  from http://github.com/bcatherall/dp-rails_app_templates/raw/master/base.rb'"
+  git :commit => '-m "Initial commit -  from http://github.com/bcatherall/dp-rails_app_templates/raw/master/base.rb"'
 
 # Success
   puts "SUCCESS!"
